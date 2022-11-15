7ff8be186be5b86ef80a4a85833019caa3126c1c
Samuel Walker <sfxworks@gmail.com>
2022-11-15 04:31:44 -0500

    IPMI

---
+++ IPMI/build/airootfs/root/init.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+echo Init success >> /tmp/log.txt
+
+if ls -lah /dev/nvme0n1; then
+  echo "Using an nvme." >> /tmp/log.txt
+  export DISK=/dev/nvme0n1 
+  sgdisk --zap-all $DISK
+  blkdiscard $DISK
+else
+  echo "Using the first sata device." >> /tmp/log.txt
+  export DISK=/dev/sda
+  sgdisk --zap-all $DISK
+  dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
+fi
+
+
+mkfs.ext4 $DISK
+mount $DISK /mnt
+
+systemctl enable --now crio 
+export UUID=$(cat /sys/class/dmi/id/board_serial)
+envsubst < /etc/kubeadm/kubeadm.conf.yaml | sponge /etc/kubeadm/kubeadm.conf.yaml
+echo "uor-$UUID" > /etc/hostname
+echo "127.0.0.1 uor-$UUID" > /etc/hosts
+echo "::1 uor-$UUID" >> /etc/hosts
+
+
+modprobe br_netfilter
+modprobe ceph
+echo '1' > /proc/sys/net/ipv4/ip_forward
+
+
+
+
+kubeadm join --config /etc/kubeadm/kubeadm.conf.yaml
+echo "Join operation complete." >> /tmp/log.txt
