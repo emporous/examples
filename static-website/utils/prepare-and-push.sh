0b90f53727a95315500d3c03ee87eafb9dc63e07
d10n <d10n@redhat.com>
2023-01-19 11:09:49 -0500

    WIP: emporous usage update

--- static-website/utils/prepare-and-push.sh
+++ static-website/utils/prepare-and-push.sh
@@ -1,10 +1,15 @@
 #/bin/sh
+
+script_dir="$(cd "${BASH_SOURCE[0]%/*}"; pwd -P)"
+
 set -e
 set -x
 # Prepare
 
-read -p 'Registry (e.g. localhost:5000/d10n/uor-framework-examples): ' registry
+read -p 'Registry (e.g. localhost:5000/emporous/emporous-framework-examples): ' registry
+registry=localhost:5000/emporous/emporous-framework-examples
 read -p "Extra Flags (e.g. --plain-http): " flags
+flags='--plain-http --insecure'
 
 
 pushd demo-content/dev
@@ -16,5 +21,13 @@ git submodule update --init --recursive
 hugo -D
 popd
 
-emporous build collection --dsconfig dataset-config.yaml demo-content $registry:static-website
+schemaAddress="${registry%/*}/staticwebsiteschema:latest"
+#emporous build schema schema-config.yaml $registry-schema:latest
+emporous build schema schema-config.yaml ${registry%/*}/staticwebsiteschema:latest
+#emporous push $flags $registry-schema:latest
+emporous push $flags "$schemaAddress"
+set -v
+(schemaAddress="$schemaAddress" envsubst <dataset-config.yaml-template >dataset-config-new.yaml)
+#emporous build collection --dsconfig <(cd "$script_dir"; schemaAddress="$schemaAddress" envsubst <../dataset-config.yaml-template) demo-content $registry:static-website
+emporous --loglevel debug build collection --no-verify --plain-http --dsconfig dataset-config-new.yaml demo-content $registry:static-website
 emporous push $flags $registry:static-website
--- static-website/utils/pull-and-serve.sh
+++ static-website/utils/pull-and-serve.sh
@@ -1,6 +1,7 @@
-#/bin/sh
+#/bin/bash
 set -e
 
+script_dir="$(cd "${BASH_SOURCE[0]%/*}"; pwd -P)"
 rm -rf /tmp/static-site
 mkdir -p /tmp/static-site
 pushd /tmp/static-site
@@ -8,9 +9,14 @@ pushd /tmp/static-site
 read -p 'Registry (e.g. localhost:5000/emporous/examples): ' registry
 read -p "Release version (dev|prod): " release
 read -p "Extra Flags (e.g. --no-verify --plain-http): " flags
+registry=localhost:5000/emporous/emporous-framework-examples
+flags='--no-verify --plain-http'
 
 echo "Pulling content"
-( set -x; emporous pull $flags --attributes release=$release $registry:static-website -o .; )
+( set -x;
+release=$release envsubst <"$script_dir/../attribute-query.yaml-template" >"$script_dir/../attribute-query-new.yaml"
+emporous pull $flags --attributes "$script_dir/../attribute-query-new.yaml" $registry:static-website -o .; )
+#( set -x; emporous pull $flags --attributes <(cd "$script_dir"; release=$release envsubst <../attribute-query.yaml-template) $registry:static-website -o .; )
 
 echo "Serving demo content on http://localhost:8081 with attributes --- ctrl+c to stop."
 
